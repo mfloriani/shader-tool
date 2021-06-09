811436efbcafb5f5d081658a932f55aac51c0c22
Murilo F Floriani <murilo.floriani@gmail.com>
2021-06-09 14:08:14 +0100

    created project structure and added spdlog

---
+++ premake5.lua
@@ -0,0 +1,72 @@
+workspace "Shadertool"
+	architecture "x64"
+	startproject "Shadertool"
+	
+	configurations
+	{
+		"Debug",
+		"Release",
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.architecture}"
+
+project "Shadertool"
+	location "Shadertool"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "pch.h"
+	pchsource "src/pch.cpp"
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+		"Common/spdlog/include",
+	}
+
+	libdirs
+	{
+		
+	}
+
+	links
+	{
+		"d3d12.lib",
+		"dxgi.lib"
+	}
+
+	filter "system:windows"
+		staticruntime "on"
+		systemversion "latest"
+
+		defines
+		{
+			
+		}
+
+	filter "configurations:Debug"
+		defines 
+		{
+			"_DEBUG"
+		}
+		symbols "on"
+		buildoptions "/MDd"
+
+	filter "configurations:Release"
+		defines 
+		{
+			
+		}
+		optimize "on"
+		buildoptions "/MD"
